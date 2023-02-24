b6fe15089c899505fd6b6c74757c21832fab85ce
maksimpetruhin <petruhinmaxim@gmail.com>
2023-02-24 15:52:26 +0300

    fix texts

--- src/scenes/l10n/VpnL10nRu.ts
+++ src/scenes/l10n/VpnL10nRu.ts
@@ -1,5 +1,5 @@
-import {VpnL10n} from './VpnL10n'
-import * as s from '../../model/scene-types'
+import {VpnL10n} from `./VpnL10n`
+import * as s from `../../model/scene-types`
 import {escapeString} from "../text-util";
 
 export class VpnL10nRu implements VpnL10n {
@@ -7,109 +7,109 @@ export class VpnL10nRu implements VpnL10n {
     start(scene: s.Start): string {
         let userName = scene.userName
         if (!userName) {
-            userName = 'Симпотяга'
+            userName = `Симпотяга`
         }
         return `Привет, ${userName}.\n` +
-            'Я помогу тебе настроить VPN на твоем устройстве.\n' +
+            `Я помогу тебе настроить VPN на твоем устройстве.\n` +
             `Для этого просто скачай конфиги, нажав на кнопку ниже, и переходи к соответствующей инструкции.\n` +
-            'Не забудь ознакомиться с дополнительной информацией.'
+            `Не забудь ознакомиться с дополнительной информацией.`
     }
 
     iphoneInstruction(scene: s.IphoneInstruction): string {
-        return 'Сообщением выше отправлена видеоинструкция для настройки VPN на Iphone или воспользуйтесь текстовой:\n \n'  +
+        return `Сообщением выше отправлена видеоинструкция для настройки VPN на Iphone или воспользуйтесь текстовой:\n \n`  +
             "1) Скачайте и установите клиент OpenVPN из AppStore. [Ссылка для скачивания](https://apps.apple.com/ru/app/openvpn-connect/id590379981)\n \n" +
-            '2) В Telegram нажмите на отправленный ранее файл "mobilecontig.ovpn" => Выберите "Сохранить в файлы" => Нажмите "Сохранить" \n \n' +
-            '3) Откройте приложение "Файлы" => Найдите файл "mobilecontig.ovpn" и откройте его с помощью программы OpenVPN' +
-            ' => Нажмите "AUD" => Нажмите "CONNECT" => Во всплывающем окне нажмите "Разрешить" \n \n' +
-            'VPN подключен. На забывайте отключать VPN, когда в нем нет потребности'
+            `2) В Telegram нажмите на отправленный ранее файл "mobilecontig.ovpn" => Выберите "Сохранить в файлы" => Нажмите "Сохранить" \n \n` +
+            `3) Откройте приложение "Файлы" => Найдите файл "mobilecontig.ovpn" и откройте его с помощью программы OpenVPN` +
+            ` => Нажмите "AUD" => Нажмите "CONNECT" => Во всплывающем окне нажмите "Разрешить" \n \n` +
+            `VPN подключен. На забывайте отключать VPN, когда в нем нет потребности`
     }
 
     macInstruction(scene: s.MacInstruction): string {
-        return 'Сообщением выше отправлена видеоинструкция для настройки VPN на MAC или воспользуйтесь текстовой:\n \n'  +
+        return `Сообщением выше отправлена видеоинструкция для настройки VPN на MAC или воспользуйтесь текстовой:\n \n`  +
             "1) Скачайте и установите клиент OpenVPN. [Ссылка для скачивания](https://openvpn.net/downloads/)\n \n" +
-            '2) В Telegram нажмите на отправленный ранее файл "pccontiq.ovpn" и откройте с помощью программы OpenVPN =>' +
-            ' на вопрос "Import .ovpn profile" нажмите "ОК" => нажмите "CONNECT"  \n \n' +
-            'VPN подключен. На забывайте отключать VPN, когда в нем нет потребности'
+            `2) В Telegram нажмите на отправленный ранее файл "pccontiq.ovpn" и откройте с помощью программы OpenVPN =>` +
+            ` на вопрос "Import .ovpn profile" нажмите "ОК" => нажмите "CONNECT"  \n \n` +
+            `VPN подключен. На забывайте отключать VPN, когда в нем нет потребности`
     }
 
     androidInstruction(scene: s.AndroidInstruction): string {
-        return 'Сообщением выше отправлена видеоинструкция для настройки VPN на Android или воспользуйтесь текстовой:\n \n'  +
+        return `Сообщением выше отправлена видеоинструкция для настройки VPN на Android или воспользуйтесь текстовой:\n \n`  +
             "1) Скачайте и установите клиент OpenVPN из PlayStore. [Ссылка для скачивания](https://play.google.com/store/apps/details?id=net.openvpn.openvpn)\n \n" +
-            '2) В Telegram нажмите на отправленный ранее файл "mobilecontig.ovpn" => Выберите "Сохранить в загрузки => ' +
-            'Откройте приложение OpenVPN => Перейдите в в раздел "Import ProFile" => "File" => "Browser" => ' +
-            '"Скаченные" => Выберите "mobilecontig.ovpn" => на вопрос "Import .ovon protile" нажмите "ОК" => ' +
-            'Установите галочку "Connect after import" и нажмите "ADD" \n \n' +
-            'VPN подключен. На забывайте отключать VPN, когда в нем нет потребности'
+            `2) В Telegram нажмите на отправленный ранее файл "mobilecontig.ovpn" => Выберите "Сохранить в загрузки => ` +
+            `Откройте приложение OpenVPN => Перейдите в в раздел "Import ProFile" => "File" => "Browser" => ` +
+            `"Скаченные" => Выберите "mobilecontig.ovpn" => на вопрос "Import .ovon protile" нажмите "ОК" => ` +
+            `Установите галочку "Connect after import" и нажмите "ADD" \n \n` +
+            `VPN подключен. На забывайте отключать VPN, когда в нем нет потребности`
     }
 
     windowsInstruction(scene: s.WindowsInstruction): string {
-        return 'Сообщением выше отправлена видеоинструкция для настройки VPN на Windows или воспользуйтесь текстовой:\n \n'  +
+        return `Сообщением выше отправлена видеоинструкция для настройки VPN на Windows или воспользуйтесь текстовой:\n \n`  +
             "1) Скачайте и установите клиент OpenVPN. [Ссылка для скачивания](https://openvpn.net/downloads/openvon-connect-v3-windows.msI)\n \n" +
-            '2) В Telegram нажмите на отправленный ранее файл "pccontiq.ovpn" и откройте с помощью программы OpenVPN => ' +
-            'на вопрос "Import .ovpn profile" нажмите "ОК" => нажимаем "CONNECI"\n \n' +
-            'VPN подключен. На забывайте отключать VPN, когда в нем нет потребности'
+            `2) В Telegram нажмите на отправленный ранее файл "pccontiq.ovpn" и откройте с помощью программы OpenVPN => ` +
+            `на вопрос "Import .ovpn profile" нажмите "ОК" => нажимаем "CONNECI"\n \n` +
+            `VPN подключен. На забывайте отключать VPN, когда в нем нет потребности`
     }
 
     generalInfo(scene: s.GeneralInfo): string {
-        return ('1) Технические характиристики: \n - входящая скорость 10-50 Mbit \n - исходящая скорость 20-50 Mbit \n ' +
-            '- время отклика 100 ms \n - установка соединения до 3 s   \n \n' +
+        return (`1) Технические характиристики: \n - входящая скорость 10-50 Mbit \n - исходящая скорость 20-50 Mbit \n ` +
+            `- время отклика 100 ms \n - установка соединения до 3 s   \n \n` +
             `2) Месячный трафик на 1 пользователя 32GB. [Инструкция](https://www.youtube.com/watch?v=_a7QSX1EkxI&ab_channel=JustUse)`+
             ` по автоматическому запуску VPN для интересующих приложений \n \n` +
-            '3) В случае падения скорости соединения, сязанную с ростом числа пользователей, ' +
-            'запустим дополнительные сервера с платной подпиской \n \n' +
-            'Администрирование серверов @ASLomonosov \n' +
-            'Архитектурные решения @aaltergot \n' +
-            'Разработка ботов @petruhinMaks \n'
+            `3) В случае падения скорости соединения, сязанную с ростом числа пользователей, ` +
+            `запустим дополнительные сервера с платной подпиской \n \n` +
+            `Администрирование серверов @ASLomonosov \n` +
+            `Архитектурные решения @aaltergot \n` +
+            `Разработка ботов @petruhinMaks \n`
             )
     }
 
     //navigation
     goToMainMenu(): string {
         return escapeString(
-            'Главное меню'
+            `Главное меню`
         )
     }
 
     goToIphoneInstruction(): string {
         return escapeString(
-            'Инструкция Iphone'
+            `Инструкция Iphone`
         )
     }
 
     goToMacInstruction(): string {
         return escapeString(
-            'Инструкция Mac'
+            `Инструкция Mac`
         )
     }
 
     goToAndroidInstruction(): string {
         return escapeString(
-            'Инструкция Android'
+            `Инструкция Android`
         )
     }
 
     goToWindowsInstruction(): string {
         return escapeString(
-            'Инструкция Windows'
+            `Инструкция Windows`
         )
     }
 
     goToGeneralInfo(): string {
         return escapeString(
-            'Дополнительная информация'
+            `Дополнительная информация`
         )
     }
 
     goToGetConfigs(): string {
         return escapeString(
-            'Скачать конфиги'
+            `Скачать конфиги`
         )
     }
 
     getText(scene: s.Scene): string {
-        let text = ''
+        let text = ``
         switch (scene.tpe) {
-            case 'Start':
+            case `Start`:
                 text = this.start(scene)
                 break
             case "IphoneInstruction":
