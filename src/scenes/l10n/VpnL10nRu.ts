67421a9af97acd5d923d5571e65617af4800cb29
maksimpetruhin <petruhinmaxim@gmail.com>
2023-07-03 19:55:30 +0300

    fix text

--- src/scenes/l10n/VpnL10nRu.ts
+++ src/scenes/l10n/VpnL10nRu.ts
@@ -16,7 +16,7 @@ export class VpnL10nRu implements VpnL10n {
     }
 
     iphoneInstruction(scene: s.IphoneInstruction): string {
-        return `Сообщением выше ближайшее время будет отправлена видеоинструкция для настройки VPN на Iphone или воспользуйтесь текстовой:\n \n`  +
+        return `Сообщением выше в ближайшее время будет отправлена видеоинструкция для настройки VPN на Iphone или воспользуйтесь текстовой:\n \n`  +
             "1) Скачайте и установите клиент OpenVPN из AppStore. [Ссылка для скачивания](https://apps.apple.com/ru/app/openvpn-connect/id590379981)\n \n" +
             `2) В Telegram нажмите на скаченный ранее файл "mobilecontig.ovpn" => Выберите "Сохранить в файлы" => Нажмите "Сохранить" \n \n` +
             `3) Откройте приложение "Файлы" => Найдите файл "mobilecontig.ovpn" и откройте его с помощью программы OpenVPN` +
@@ -25,7 +25,7 @@ export class VpnL10nRu implements VpnL10n {
     }
 
     macInstruction(scene: s.MacInstruction): string {
-        return `Сообщением выше ближайшее время будет отправлена видеоинструкция для настройки VPN на MAC или воспользуйтесь текстовой:\n \n`  +
+        return `Сообщением выше в ближайшее время будет отправлена видеоинструкция для настройки VPN на MAC или воспользуйтесь текстовой:\n \n`  +
             "1) Скачайте и установите клиент OpenVPN. [Ссылка для скачивания](https://openvpn.net/client-connect-vpn-for-mac-os/)\n \n" +
             `2) В Telegram нажмите на скаченный ранее файл "pccontiq.ovpn" и откройте с помощью программы OpenVPN =>` +
             ` на вопрос "Import .ovpn profile" нажмите "ОК" => нажмите "CONNECT"  \n \n` +
@@ -33,7 +33,7 @@ export class VpnL10nRu implements VpnL10n {
     }
 
     androidInstruction(scene: s.AndroidInstruction): string {
-        return `Сообщением выше ближайшее время будет отправлена видеоинструкция для настройки VPN на Android или воспользуйтесь текстовой:\n \n`  +
+        return `Сообщением выше в ближайшее время будет отправлена видеоинструкция для настройки VPN на Android или воспользуйтесь текстовой:\n \n`  +
             "1) Скачайте и установите клиент OpenVPN из PlayStore. [Ссылка для скачивания](https://play.google.com/store/apps/details?id=net.openvpn.openvpn)\n \n" +
             `2) В Telegram нажмите на скаченный ранее файл "mobilecontig.ovpn" => Выберите "Сохранить в загрузки"\n \n` +
             `3) Откройте приложение OpenVPN => Перейдите в в раздел "Import ProFile" => "File" => "Browser" => ` +
